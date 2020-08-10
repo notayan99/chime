4e2dfd621067824a72f4ddc7bfd3a88845d02362
Yo Chiang <yochiang@google.com>
2020-08-10 17:28:04 +0800

    chime: Enable mainline artifact path enforcement
    Bug: 138706293
    Test: TH / Forrest build pass
    Change-Id: Ife32e2308f54817e7fd0393a9e9c6e8b02ed8f92
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>
    Signed-off-by: Raya Mahardika (rktdnt.) <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable mainline artifact path enforcement
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
