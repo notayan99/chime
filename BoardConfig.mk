8f1a32ffdcde91284f1e5f17533c7b420c8b1f29
frost. (frostg-012) <frost@nusantararom.org>
2022-09-15 13:47:30 +0000

    chime: Build GPS stack from source
     * also, move gps configs into gps/etc/.
    
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I0a331cb3edb9ba277119cf8f11890c1bbb7bb31c
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>
    Signed-off-by: Raya Mahardika (rktdnt.) <rktdnt@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,10 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
